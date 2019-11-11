0c2c83cd9885d8348e5ee4206aecb2a1a7281eb2
romankalab <roan.kalab@mail.com>
2019-11-11 23:18:42 +0100

    Initial

---
+++ Open-Lab-04.13/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Open_Lab_04._13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two strings");
+            string str1 = Console.ReadLine();
+            string str2 = Console.ReadLine();
+
+            StringTools stringtools = new StringTools();
+            Console.WriteLine();
+            Console.WriteLine("This is result:" + " " + stringtools.GetLongestCommonSequence(str1, str2));
+        }
+    }
+}
---
+++ Open-Lab-04.13/StringTools.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open_Lab_04._13
+{
+    class StringTools
+    {
+        public string GetLongestCommonSequence(string str1, string str2)
+        {
+            var result = "";
+
+            var builder = new StringBuilder();
+            for (var x = 0; x < str1.Length; x++)
+            {
+                var pos = x;
+                foreach (var ch in str2)
+                {
+                    if (pos < str1.Length && str1[pos] == ch)
+                    {
+                        builder.Append(ch);
+                        ++pos;
+                    }
+                    else
+                    {
+                        if (builder.Length > result.Length)
+                            result = builder.ToString();
+                        builder.Clear();
+                    }
+                }
+
+                if (builder.Length > result.Length)
+                {
+                    result = builder.ToString();
+                    builder.Clear();
+                }
+            }
+            return result;
+        }
+    }
+}
